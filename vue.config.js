756ec7d642112a2a2ac8c7ecb8bbe5691620b32e
U-Wen <1312389145@qq.com>
2020-12-23 16:19:01 +0800

    second-commit alter-title-icon

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,25 @@
-const path = require('path')
-
-const resolve = dir => {
-  return path.join(__dirname, dir)
-}
-
 module.exports = {
-  chainWebpack: config => {
-    config.resolve.alias
-      .set('@', resolve('./src'))
-      .set('assets', resolve('.src/assets'))
-      .set('commonJS', resolve('.src/commonJS'))
-      .set('components', resolve('.src/components'))
-      .set('network', resolve('.src/network'))
-      .set('views', resolve('.src/views'))
+  publicPath: '/',
+  outputDir: 'dist',
+  pwa: {
+    iconPaths: {
+      favicon32: 'favicon.ico',
+      favicon16: 'favicon.ico',
+      appleTouchIcon: 'favicon.ico',
+      maskIcon: 'favicon.ico',
+      msTileImage: 'favicon.ico'
+    }
+  },
+  devServer: {
+    proxy: 'http://192.168.55.197:8080/'
   }
+  // chainWebpack: config => {
+  //   config.resolve.alias
+  //     .set('@', resolve('./src'))
+  //     .set('assets', resolve('./src/assets'))
+  //     .set('commonJS', resolve('./src/commonJS'))
+  //     .set('components', resolve('./src/components'))
+  //     .set('network', resolve('./src/network'))
+  //     .set('views', resolve('./src/views'))
+  // }
 }
